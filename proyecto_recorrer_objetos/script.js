3ecfbc311d265b0c582944b2989712ab2bc988be
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2022-07-19 11:52:50 +0200

    feat proyecto_recorrer

--- proyecto_recorrer_objetos/script.js
+++ proyecto_recorrer_objetos/script.js
@@ -1,46 +1,177 @@
-//Definimos nuestro array de datos
-const list = [
+//DEFINIMOS NUESTROS ARRAYS DE DATOS
+const users = [
   {
-    name: "Objeto 1",
-    image:
-      "https://www.pngitem.com/pimgs/m/35-350426_profile-icon-png-default-profile-picture-png-transparent.png",
-    id: 1,
+    name: "Miss Marvin Wiza",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1231.jpg",
+    email: "Art32@yahoo.com"
   },
   {
-    name: "Objeto 2",
-    image:
-      "https://www.pngitem.com/pimgs/m/35-350426_profile-icon-png-default-profile-picture-png-transparent.png",
-    id: 2,
+    name: "Mrs. Garry Witting",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/364.jpg",
+    email: "Verdie_Luettgen23@yahoo.com",
+    active: true,
+  },
+  {
+    name: "Patricia Schneider",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/996.jpg",
+    email: "Fay.Emard@hotmail.com",
+    active: true,
+  },
+  {
+    name: "Daisy Hand",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/684.jpg",
+    email: "Emory38@yahoo.com",
+    active: true,
+  },
+  {
+    name: "Stella Ritchie",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/562.jpg",
+    email: "Trace.Pagac@gmail.com",
+    active: true,
+  },
+  {
+    name: "Clayton Smitham",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1229.jpg",
+    email: "Mafalda_Prohaska22@hotmail.com",
+    active: true,
+  },
+  {
+    name: "Mrs. Beulah Turcotte",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/435.jpg",
+    email: "Jarrod_Daniel@yahoo.com",
+    active: true,
+  },
+  {
+    name: "Maxine Cruickshank",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/851.jpg",
+    email: "Javonte.Kuphal74@gmail.com",
+    active: true,
+  },
+  {
+    name: "Eileen Roberts",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/285.jpg",
+    email: "Margarita_Dicki@hotmail.com",
+    active: true,
+  },
+  {
+    name: "Ms. Glen Dickinson",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/601.jpg",
+    email: "Joelle90@gmail.com"
+  },
+];
+const animals = [
+  {
+    name: "Galgo Español",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Kasey_Shields94@example.com",
+    active: true,
+  },
+  {
+    name: "Cimarrón Uruguayo",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Wilfred_Considine45@example.org",
+    active: true,
+  },
+  {
+    name: "Sealyham Terrier",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Trisha94@example.org",
+    active: true,
+  },
+  {
+    name: "Shiloh Shepherd",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Jarred.Terry52@example.org",
+    active: true,
+  },
+  {
+    name: "Basset Bleu de Gascogne",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Yolanda61@example.net",
+    active: true,
+  },
+  {
+    name: "Sarabi dog",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Cory_Schulist40@example.org",
+    active: true,
+  },
+  {
+    name: "Volpino Italiano",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Eloy49@example.net",
+    active: true,
+  },
+  {
+    name: "Hierran Wolfdog",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Ava58@example.org",
+    active: true,
+  },
+  {
+    name: "Nova Scotia Duck Tolling Retriever",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Beaulah3@example.net",
+    active: true,
+  },
+  {
+    name: "Dogo Sardesco",
+    avatar: "http://loremflickr.com/640/480/animals",
+    email: "Jovany_Ullrich88@example.com"
   },
 ];
 
-//Recuperamos la etiqueta de nuestro documento HTML
-const divContainer = document.querySelector("#container");
+//1. Recuperar los divs contenedores del HTML
+//2. Recorrer los arrays
+//3. Generar un elemento de HTML
+//4. Reflejar este elemento de HTML en el contenedor
 
-//Iteramos el array
-const iterateArray = (array, container) => {
-  for (const item of array) {
-    generateHTML(item, container);
-    //Ejecutamos dentro del bucle la función que genera el bloque de HTML
+//1. Recuperamos los contenedores de HTML
+const usersContainer = document.querySelector("#users_container");
+const animalsContainer = document.querySelector("#animals_container");
+
+//2. Iteramos x lista mediante un bucle y sacamos los elementos individuales de ella. Además pasamos el contenedor para poder utilizarlo en el paso 4.
+const iterateArrays = (list, container) => {
+  for (const element of list) {
+    //Por cada uno de los elementos individuales generamos un bloque de HTML, y además pasamos el contenedor a la siguiente función hasta llegar al paso 4.
+    generateHTML(element, container);
   }
 };
 
-//Generamos un bloque de HTML recuperando la información extraida de la iteración
-const generateHTML = (item, container) => {
-  const figure = `
+//3. Generamos por cada uno de los elementos del bucle del paso 2 un <figure> y pintamos las propiedades de cada uno de los objetos. Lo almacenamos en una constante para poder pasarla por argumento a la función del paso 4.
+const generateHTML = (element, container) => {
+  let myFigure;
+  //Con este condicional comprobamos si cada uno de los objetos tiene la clave active, y si no la tiene myFigure va a ser vacio, por lo tanto no va a pintar nada
+  if ("active" in element) {
+    myFigure = `
     <figure>
-        <h2>${item.name}</h2>
-        <img src="${item.image}" alt="${item.name}" width="300px" />
-        <p>${item.id}</p>
+    <h3>${element.name}</h3>
+    <p>${element.email}</p>
+    <img src="${element.avatar}" alt="${element.name}" />
     </figure>`;
-  print(figure, container);
-  //Ejecutamos la función que pinta los elementos en
+  } else{
+    myFigure = ``
+  }
+
+  //Le pasamos tanto el tag que vamos a pintar en el HTML como el contenedor que llevamos arrastrando desde el paso 1, porque esta es la función que lo va a utilizar
+  print(myFigure, container);
 };
 
-//Pintamos los elementos que hemos generado en el contenedor que hemos creado
-const print = (element, container) => {
-  container.innerHTML += element;
+//4. Nos traemos el tag de la función y le indicamos el contenedor donde la vamos a pintar con el metodo innerHTML.
+const print = (figure, container) => {
+  container.innerHTML += figure;
 };
 
-//Ejecutamos la primera función que itera la lista, la cual desencadenará el resto de funciones tal como se indica en cada uno de los pasos
-iterateArray(list, divContainer);
+//EJECUTAR LA FUNCION PRINCIPAL
+iterateArrays(users, usersContainer);
+iterateArrays(animals, animalsContainer);
